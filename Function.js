05e956351debfa4dc48eafdccd511543ee8ffde6
RakibulRanak <43205114+RakibulRanak@users.noreply.github.com>
2020-05-01 02:30:56 +0600

    Create Function.js

---
+++ Function.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    return a + b;
+}
+
+const sum = function (a, b) {
+    return a + b;
+}
+
+console.log(add(5, 5));
+console.log(sum(10, 10));
+
+//ES6
+
+const mult = (a, b) => {
+    return a * b;
+}
+console.log(mult(4, 5));
